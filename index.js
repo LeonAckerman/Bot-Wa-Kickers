59631ee699851a78dbd846e106d26a4cb57a0c20
LeonAckerman <mzulfiridwan57@gmail.com>
2025-04-11 01:10:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ const toxicKeywords = [
 const strikeMap = {};
 
 const { useSingleFileAuthState } = require('@whiskeysockets/baileys');
-const { state, saveState } = useSingleFileAuthState('./auth_info.json');
 
 async function startSock() {
   const sock = makeWASocket({
